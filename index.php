e03ebb5c087c3c4807e8f5e3f8dcba1b28cdd375
MCN <phongnhadn1991@gmail.com>
2019-10-25 01:34:46 +0700

    toppage

--- index.php
+++ index.php
@@ -14,46 +14,72 @@
 <div id="abi_page">
 <?php include LOCATION_ROOT_DIR."/templates/header.php"; ?>
 <div id="contents">
+	<section class="con_about">
+		<div class="container">
+			<div class="wrap_about">
+				<div class="box_left">
+					<div class="box_txt js-inview anime-fade-in">
+						<h2 class="tit_st center">
+							<span class="sup">Về chúng tôi</span>
+							<span class="main">HiềnLee Makeup</span>
+						</h2>
+						<p class="txt">
+							Lựa chọn học tại Trang điểm chuyên nghiệp tại HiềnLee Makeup bạn sẽ nhận được những lợi thế mà không phải nơi nào cũng có thể mang lại cho các học viên.
+							Đầu tiên chúng tôi xây dựng chương trình các khóa học rất chuyên nghiệp và rõ ràng để các học viên biết rõ mình sẽ học được gì sau mỗi khóa học.
+							Tiếp theo chúng tôi luôn cập nhật những xu hướng trang điểm mới nhất để truyền lại cho học viên, hệ thống máy móc tiên tiến cũng được trang bị để hỗ trợ quá trình học tập thuận lợi hơn.
+						</p>
+						<p class="btn btn__ghost--prm btn--small"><a href="#77777" target="_blank"><span>Xem chi tiết</span></a></p>
+					</div>
+				</div>
+				<div class="box_right">
+					<div class="box_img">
+						<p class="img shine js-inview anime-fade-in u-delay1"><img src="<?php echo location_file ?>images/home/img_about1.jpg" alt="images"></p>
+						<p class="img shine js-inview anime-fade-in u-delay2"><img src="<?php echo location_file ?>images/home/img_about2.jpg" alt="images"></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- /.con_about -->
 
 	<section class="con_activity">
 		<article class="box_activity">
 			<ul>
-				<li class="js-inview anime-fade-in">
+				<li class="js-inview anime-fade-in u-delay1">
 					<a href="#">
 						<p class="img"><img src="<?php echo location_file ?>images/home/img_activity1.jpg" alt="images"></p>
 						<div class="box_txt">
 							<p class="catch">Trang điểm sự kiện, <br> dự tiệc, cô dâu..</p>
-							<svg><use xlink:href="#ic_watch_nav"></svg>
+							<p class="img_ic"><img src="<?php echo location_file ?>images/home/icon_mk_1.png" alt="images"></p>
 							<h3>Makeup</h3>
 						</div>
 					</a>
 				</li>
-				<li class="js-inview anime-fade-in">
+				<li class="js-inview anime-fade-in u-delay2">
 					<a href="#">
 						<p class="img"><img src="<?php echo location_file ?>images/home/img_activity2.jpg" alt="images"></p>
 						<div class="box_txt">
 							<p class="catch">Tham gia các sự kiện lớn <br> của HieenLee Makeup</p>
-							<svg class="play"><use xlink:href="#ic_play"></svg>
+							<p class="img_ic"><img src="<?php echo location_file ?>images/home/icon_mk_2.png" alt="images"></p>
 							<h3>HiềnLee Makeup</h3>
 						</div>
 					</a>
 				</li>
-				<li class="js-inview anime-fade-in">
+				<li class="js-inview anime-fade-in u-delay3">
 					<a href="#">
 						<p class="img"><img src="<?php echo location_file ?>images/home/img_activity3.jpg" alt="images"></p>
 						<div class="box_txt">
 							<p class="catch">Đào tạo & hướng dẫn<br> chuyên nghiệp</p>
-							<svg><use xlink:href="#ic_dish"></svg>
+							<p class="img_ic"><img src="<?php echo location_file ?>images/home/icon_mk_3.png" alt="images"></p>
 							<h3>Khóa học Makeup</h3>
 						</div>
 					</a>
 				</li>
-				<li class="js-inview anime-fade-in">
+				<li class="js-inview anime-fade-in u-delay4">
 					<a href="#">
 						<p class="img"><img src="<?php echo location_file ?>images/home/img_activity4.jpg" alt="images"></p>
 						<div class="box_txt">
 							<p class="catch">Trải nghiệm thực tế <br> & việc làm</p>
-							<svg><use xlink:href="#ic_stay"></svg>
+							<p class="img_ic"><img src="<?php echo location_file ?>images/home/icon_mk_4.png" alt="images"></p>
 							<h3>Việc làm</h3>
 						</div>
 					</a>
@@ -61,6 +87,70 @@
 			</ul>
 		</article><!-- /.box_activity -->
 	</section> <!-- /.con_activity -->
+
+	<section class="con_service">
+		<div class="wrap_service">
+			<div class="box_head">
+				<h2 class="tit_st center">
+					<span class="sup">Dịch vụ trang điểm</span>
+					<span class="main">HiềnLee Makeup</span>
+				</h2>
+			</div>
+			<div class="box_list">
+				<div class="item js-inview anime-fade-in u-delay1">
+					<a href="#">
+						<div class="box_img">
+							<p class="img shine"><img src="<?php echo location_file ?>images/home/img_service1.jpg" alt="images"></p>
+						</div>
+						<div class="box_txt">
+							<p class="txt_st">Trang điểm beauty</p>
+						</div>
+					</a>
+				</div>
+				<div class="item js-inview anime-fade-in u-delay2">
+					<a href="#">
+						<div class="box_img">
+							<p class="img shine"><img src="<?php echo location_file ?>images/home/img_service2.jpg" alt="images"></p>
+						</div>
+						<div class="box_txt">
+							<p class="txt_st">Trang điểm cô dâu hỏi / ngoại cảnh</p>
+						</div>
+					</a>
+				</div>
+				<div class="item js-inview anime-fade-in u-delay3">
+					<a href="#">
+						<div class="box_img">
+							<p class="img shine"><img src="<?php echo location_file ?>images/home/img_service3.jpg" alt="images"></p>
+						</div>
+						<div class="box_txt">
+							<p class="txt_st">Trang điểm cô dâu ngày cưới</p>
+						</div>
+					</a>
+				</div>
+				<div class="item js-inview anime-fade-in u-delay4">
+					<a href="#">
+						<div class="box_img">
+							<p class="img shine"><img src="<?php echo location_file ?>images/home/img_service4.jpg" alt="images"></p>
+						</div>
+						<div class="box_txt">
+							<p class="txt_st">Trang điểm sự kiện / Mc</p>
+						</div>
+					</a>
+				</div>
+				<div class="item js-inview anime-fade-in u-delay5">
+					<a href="#">
+						<div class="box_img">
+							<p class="img shine"><img src="<?php echo location_file ?>images/home/img_service5.jpg" alt="images"></p>
+						</div>
+						<div class="box_txt">
+							<p class="txt_st">Trang điểm đi tiệc / kỷ yếu</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
 <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
 <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
 <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
